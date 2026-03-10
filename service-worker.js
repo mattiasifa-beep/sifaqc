7f40f6e321b3d6b19cfeeca5152623c3d071e3e5
mattiasifa-beep <mattia.sifa@gmail.com>
2026-03-10 12:25:06 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME='sifa-qc-v2-cache';
-const urlsToCache=['./','./index.html','./manifest.json','./logo_sifa.jpg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
